b618c9246b032fc6c73fca7ad82f2d9b2c0b73bb
ruhi <ruhishreya2003@gmail.com>
2024-07-14 15:05:33 +0530

    dashboard

--- receipt/dashboard.php
+++ receipt/dashboard.php
@@ -8,43 +8,13 @@ if (!isset($_SESSION['email'])) {
     header("Location: ../index.php");
     exit(); // Ensure script stops executing after redirection
 }
-
-// Function to check overall Audit Report status
-function getAuditReportStatus($conn) {
-    // Query to fetch available quantities from audit_log table
-    $query = "SELECT SUM(audit_quantity) AS total_audit_quantity, SUM(qty_23_24) AS total_available_quantity  FROM audit_log";
-    $result = mysqli_query($conn, $query);
-    $row = mysqli_fetch_assoc($result);
-    $total_audit_quantity = (int)$row['total_audit_quantity']; // Cast to integer for safe comparison
-    $total_available_quantity = (int)$row['total_available_quantity']; // Cast to integer for safe comparison
-
-    // Calculate the difference
-    $difference = $total_audit_quantity - $total_available_quantity;
-
-    // Determine status dynamically
-    if ($difference == 0) {
-        return "Normal";
-    } else {
-        return ($difference > 0) ? "+".$difference : $difference; // Return the difference as abnormal
-    }
-}
-
-// Get overall Audit Report status
-$auditReportStatus = getAuditReportStatus($conn);
-
-    // Total stocks in inv
-
-    $query = "SELECT SUM(available_quantity) AS total_stocks FROM inv_location";
-    $result = $conn->query($query);
-    $row = $result->fetch_assoc();
-    $total_stocks = $row['total_stocks'];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Inv Dashboard</title>
+    <title>Pickpack Dashboard</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -52,8 +22,7 @@ $auditReportStatus = getAuditReportStatus($conn);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <style>
         html,
         body,
@@ -74,7 +43,6 @@ $auditReportStatus = getAuditReportStatus($conn);
         <span class="w3-bar-item w3-left"><a href="index.php" style="text-decoration:none;">Home</a></span>
         <span class="w3-bar-item w3-right"><a href="../logout.php" style="text-decoration:none;">Logout</a></span>
     </div>
-
     <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
         <div class="w3-container w3-row">
             <div class="w3-col s8 w3-bar">
@@ -87,16 +55,18 @@ $auditReportStatus = getAuditReportStatus($conn);
         <div class="w3-container" style="padding-top:0">
         </div>
         <div class="w3-bar-block">
-            <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="material-icons" style="font-size:15px">dashboard</i>&nbsp; Overview</a>
+            <a href="dashboard.php" class="w3-bar-item w3-button w3-padding w3-blue"><i class="material-icons" style="font-size:15px">dashboard</i>&nbsp; Overview</a>
             <div style="margin-top: 10px;"></div>
-            <a href="#" class="w3-bar-item w3-button w3-padding w3-brown"><i class="fa fa-cubes" style="font-size:15px"></i>&nbsp;Ordered()</a>
+            
+            <a href="pp_orders.php" class="w3-bar-item w3-button w3-padding w3-brown"><i style="font-size:15px" class="fa">&#xf0ae;</i>&nbsp; Stock</a>
             <div style="margin-top: 10px;"></div>
-            <a href="#" class="w3-bar-item w3-button w3-padding w3-green"><i class='fa fa-line-chart' style='font-size:15px'></i>&nbsp; Received</a>
+            <a href="customer.php" class="w3-bar-item w3-button w3-padding w3-green"><i style="font-size:15px" class="fa">	&#xf0c0;</i>&nbsp; Sales</a>
             <div style="margin-top: 10px;"></div>
-            <a href="#" class="w3-bar-item w3-button w3-padding w3-red"><i class="fa fa-list" style="font-size:15px"></i>&nbsp; Audit Report ()</a>
+            <a href="customer.php" class="w3-bar-item w3-button w3-padding w3-red"><i style="font-size:15px" class="fa">	&#xf03a;</i>&nbsp; Audit report</a>
             <div style="margin-top: 10px;"></div>
-            <a href="#" class="w3-bar-item w3-button w3-padding w3-yellow"><i class="fa fa-heartbeat" style="font-size:24px"></i>&nbsp; Safety Report()</a>
-        </div>
+            <a href="#" class="w3-bar-item w3-button w3-padding w3-yellow"><i style="font-size:15px" class="fa">&#xf21e;</i>&nbsp;Safety  Report</a>
+            <div style="margin-top: 10px;"></div>
+           
     </nav>
 
     <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
@@ -110,33 +80,32 @@ $auditReportStatus = getAuditReportStatus($conn);
         <div class="w3-row-padding w3-margin-bottom">
             <div class="w3-quarter">
                 <div class="w3-container w3-brown w3-padding-16">
-                    <div class="w3-left"><i style="font-size:58px" class="fa">&#xf1b3;</i></div>
+                    <div class="w3-left"><i style="font-size:58px" class="fa">&#xf0ae;</i></div>
                     <div class="w3-right">
                         <h3></h3>
                     </div>
                     <div class="w3-clear"></div>
-                    <h4>Ordererd()</h4>
-
+                    <h4>Stock</h4>
                 </div>
             </div>
             <div class="w3-quarter">
                 <div class="w3-container w3-green w3-padding-16">
-                    <div class="w3-left"><i style="font-size:58px" class="fa">&#xf201;</i></div>
+                    <div class="w3-left"><i style="font-size:58px" class="fa">	&#xf0c0;</i></div>
                     <div class="w3-right">
                         <h3></h3>
                     </div>
                     <div class="w3-clear"></div>
-                    <h4>Received()</h4>
+                    <h4>Sales</h4>
                 </div>
             </div>
             <div class="w3-quarter">
                 <div class="w3-container w3-red w3-padding-16">
-                    <div class="w3-left"><i style="font-size:58px" class="fa">&#xf03a;</i></div>
+                    <div class="w3-left"><i style="font-size:58px" class="fa">	&#xf03a;</i></div>
                     <div class="w3-right">
                         <h3></h3>
                     </div>
                     <div class="w3-clear"></div>
-                    <h4>Audit Report (<?php echo $auditReportStatus; ?>)</h4>
+                    <h4>Audit reports</h4>
                 </div>
             </div>
             <div class="w3-quarter">
@@ -146,38 +115,37 @@ $auditReportStatus = getAuditReportStatus($conn);
                         <h3></h3>
                     </div>
                     <div class="w3-clear"></div>
-                    <h4>Safety Report()</h4>
+                    <h4>Safety Report</h4>
                 </div>
             </div>
-        </div>
+        
         <div class="w3-container">
-            <h4>General Stats</h4>
+          <h4>General Stats</h4>
+          
+          <div class="w3-row">
+              <div class="w3-half">
+                  <div class="w3-container">
+                      <h5>Weekly Stock Movement</h5>
+                      <canvas id="stockChart" style="max-width: 100%;"></canvas>
+                  </div>
+              </div>
+              <div class="w3-half">
+                  <div class="w3-container">
+                      <h5>Top Products</h5>
+                      <canvas id="topProductsChart" style="max-width: 100%;"></canvas>
+                  </div>
+              </div>
+          </div>
+          
+          <script src="script.js"></script>
+      </div>
 
-            <div class="w3-row">
-                <div class="w3-half">
-                    <div class="w3-container">
-                        <h5>Weekly Stock Movement</h5>
-                        <canvas id="stockChart" style="max-width: 100%;"></canvas>
-                    </div>
-                </div>
-                <div class="w3-half">
-                    <div class="w3-container">
-                        <h5>Top Products</h5>
-                        <canvas id="topProductsChart" style="max-width: 100%;"></canvas>
-                    </div>
-                </div>
-            </div>
-            <script src="script.js"></script>
-        </div>
-
-        <center>
-            <br />
-            <p>2024 &#169; All Rights Reserved | Developed and Maintained by <b>Pentagon</b></p>
-
-        </center>
-
-
-        <script>
+      <center>
+        <br/>
+          <p>2024 &#169; All Rights Reserved | Developed and Maintained by <b>Pentagon</b></p>
+      
+    </center>
+    <script>
             //Real time formats
             function updateTimeAndGreeting() {
                 // Get current time
@@ -215,6 +183,8 @@ $auditReportStatus = getAuditReportStatus($conn);
             updateTimeAndGreeting();
 
         </script>
+   
+
 
 </body>
 
